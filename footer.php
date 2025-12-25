c5b01449cf43cb02139dc390442fd56d587b0e4b
Apu Chandra Sutradhar <apus081102@gmail.com>
2025-12-25 22:33:17 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer style="text-align:center; padding:15px; background:#f1f1f1; margin-top:20px;">
+    &copy; 2025 Smart Elevator Service & Management System. All Rights Reserved.
+</footer>
+</body>
+</html>
